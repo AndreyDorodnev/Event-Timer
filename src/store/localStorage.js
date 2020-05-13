9e121f48fc71e149d0dbfffcf8229df773a7bd8a
Andrey Dorodnev <andrew992@mail.ru>
2020-05-13 16:15:21 +0400

    local storage save-load methods add

---
+++ src/store/localStorage.js
@@ -0,0 +1,30 @@
+import {getNextYear,getPrevYear} from '../time';
+
+const initialSettings = {
+    darkTheme: false,
+    directCount: false,
+    stopTime: getNextYear(),
+    startTime: getPrevYear()
+}
+
+export const saveSettings = state => {
+    try{
+        const serializedSettings = JSON.stringify(state);
+        localStorage.setItem('timerState',serializedSettings);
+    } catch(error){
+        console.log('local storage save state error!');
+    }
+}
+
+export const loadSettings = () => {
+    try{
+        const serializedSettings = localStorage.getItem('timerState');
+        if(serializedSettings){
+            return JSON.parse(serializedSettings);
+        }
+        return initialSettings;
+    } catch(error){
+        console.log('local storage load state error!');
+        return initialSettings;
+    }
+}
