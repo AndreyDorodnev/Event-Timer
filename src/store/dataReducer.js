c0050a4db65451869cd70af9d5102862274179a3
Andrey Dorodnev <andrew992@mail.ru>
2020-05-15 10:02:32 +0400

    save settings add

--- src/store/dataReducer.js
+++ src/store/dataReducer.js
@@ -1,4 +1,4 @@
-import {THEME_CHANGE,START_TIME_CHANGE,COUNT_CHANGE, STOP_TIME_CHANGE} from './actionTypes';
+import {THEME_CHANGE,START_TIME_CHANGE,COUNT_CHANGE, STOP_TIME_CHANGE,SAVE_SETTINGS} from './actionTypes';
 import {loadSettings,saveSettings} from './localStorage';
 
 const initialState = loadSettings();
@@ -6,24 +6,29 @@ const initialState = loadSettings();
 export default function dataReducer(state=initialState,action){
     switch(action.type){
         case THEME_CHANGE: {
+            !state.darkTheme? document.documentElement.setAttribute('theme','dark'): document.documentElement.setAttribute('theme','light');
+            saveSettings({...state,darkTheme:!state.darkTheme});
             return {
                 ...state,
                 darkTheme: !state.darkTheme
             }
         }
         case START_TIME_CHANGE: {
+            saveSettings({...state,startTime:action.time});
             return {
                 ...state,
-                startTime: action.startTime
+                startTime: action.time
             }
         }
         case STOP_TIME_CHANGE: {
+            saveSettings({...state,stopTime: action.time});
             return {
                 ...state,
-                stopTime: action.stopTime
+                stopTime: action.time
             }
         }
         case COUNT_CHANGE: {
+            saveSettings({...state,directCount:!state.directCount});
             return {
                 ...state,
                 directCount: !state.directCount
