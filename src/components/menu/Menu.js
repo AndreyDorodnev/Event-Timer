3f016d0589b403a1db41261fd163001d33cfa43e
Andrey Dorodnev <andrew992@mail.ru>
2020-05-15 10:05:59 +0400

    add redux to menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getStartTime,getStopTime,isDarkTheme,isDirectCount} from '../../store/dataReducer';
+import {THEME_CHANGE,START_TIME_CHANGE,COUNT_CHANGE, STOP_TIME_CHANGE} from '../../store/actionTypes';
+import Classes from './menu.module.scss';
 // import Classes from './menu.module.scss';
 import MenuButton from '../menuBtn/MenuButton';
-// import ToggleButton from '../elements/ToggleButton';
+import ToggleButton from '../toggleBtn/ToggleButton';
 // import DateInput from '../elements/DateInput';
 
 import DatePicker from '../datePicker.js/DatePicker';
 
-export default props => {
-    return (
-        <div className={props.classes.join(' ')}>
-            <MenuButton btnClick={props.btnClick}></MenuButton>
-            <DatePicker caption="Start time"></DatePicker>
-            <DatePicker caption="Stop time"></DatePicker>
-            {/* <ToggleButton text={'direct mode'} active={props.directCount} buttonClick={props.toggleCount} ></ToggleButton> */}
-            {/* <ToggleButton text={'dark theme'} active={props.darkTheme} buttonClick={props.toggleTheme}></ToggleButton> */}
-            {/* <DateInput changeStopTime={props.changeStopTime}></DateInput> */}
-        </div>
-    )
+function Menu(props) {
+
+  return (
+      <div className={[Classes.menu,props.active? Classes.menuActive : ''].join(' ')}>
+          <MenuButton btnClick={props.btnClick}></MenuButton>
+          <ToggleButton text='direct mode' active={props.directCount} buttonClick={props.countModeChange} ></ToggleButton>
+          <ToggleButton text="dark theme" active={props.darkTheme} buttonClick={props.themeChange}></ToggleButton>
+          <DatePicker caption="Start time" currentTime={props.startTime} setTime={props.setStartTime}></DatePicker>
+          <DatePicker caption="Stop time" currentTime={props.stopTime} setTime={props.setStopTime}></DatePicker>
+      </div>
+  )
 }
+
+const mapStateToProps = state => {
+    return {
+        stopTime: getStopTime(state),
+        startTime: getStartTime(state),
+        darkTheme: isDarkTheme(state),
+        directCount: isDirectCount(state)
+    }
+  }
+  
+  const mapDispatchToProps = dispatch=>{
+    return {
+      setStopTime: (time) => dispatch({type:STOP_TIME_CHANGE,time}),
+      setStartTime: (time) => dispatch({type:START_TIME_CHANGE,time}),
+      themeChange: () => dispatch({type:THEME_CHANGE}),
+      countModeChange: () => dispatch({type:COUNT_CHANGE}),
+    }
+  }
+  
+  export default connect(mapStateToProps,mapDispatchToProps)(Menu);
