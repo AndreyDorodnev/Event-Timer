a72ebc54d1f0eb5aa682cd5849fd1a7a434ddb30
Andrey Dorodnev <andrew992@mail.ru>
2020-05-15 10:03:24 +0400

    date picker validation add

--- src/components/datePicker.js/DatePicker.js
+++ src/components/datePicker.js/DatePicker.js
@@ -1,26 +1,37 @@
 import React, {useState} from 'react';
 import DateInput from './DateInput';
 import Classes from './datePicker.module.scss';
+import {checkDateValid} from '../../time';
+
 export default props => {
 
     const [time,setTime] = useState({hours:'00', minutes:'00'});
     const [date,setDate] = useState('');
+    const [validateError,setValidateError] = useState(false);
+
+    const getTimeStr = () => {
+        return time.hours + ':' + time.minutes; 
+    }
 
     const dataSubmit = (event) => {
         event.preventDefault();
-        console.log('date',date);
-        console.log('time',time);
-        
+        if(checkDateValid(date)) {
+            props.setTime(date + ' ' + getTimeStr());
+            // props.saveSettings();
+            setValidateError(false);
+        } else {
+            setValidateError(true);
+        }
     }
 
     const dateChange = newDate => {
-        setDate(newDate);
+        setDate(newDate.split('').map(element=>element==='/'?'-':element).join(''));
     }
 
     return (
         <form className={Classes.datePicker} onSubmit={dataSubmit}>
-            <label>{props.caption}</label>
-            <DateInput dateChange={dateChange} validateError={false}></DateInput>
+            <label>{`${props.caption} ${props.currentTime}`}</label>
+            <DateInput dateChange={dateChange} validateError={validateError}></DateInput>
             <div className={Classes.timer}>
                 <div className={Classes.hours}>{time.hours}</div>
                 <span>:</span>
